026c5822f44b5d6a107a71b443080577612a0a81
arzafran <arzafran@gmail.com>
2014-11-07 09:18:27 -0300

    02 - OOP JS

--- 01-js-intro-and-jquery/scripts.js
+++ 01-js-intro-and-jquery/scripts.js
@@ -8,22 +8,19 @@ $(document).ready(function(){
         var arr = data.response.split(" ");
         return arr.shift() + ' <span class="higlight">' + arr.join(" ") + ' </span>';
       })
-	}, "json" )
-	.fail(function() { $("#greeter-text").addClass("error"); });
-	});
-  $('.ready-button').click(function(){
+	}, "json" ).fail(function() { $("#greeter-text").addClass("error"); });
     $.ajax({
       url: "http://localhost:3000/search?q=html5",
       type: "get"
     }).success(function(data){
       console.log(data);
       $.each(data.statuses,function(i,val){
-        var tweetBox = '<div class="tweets-unit">' +
+        var tweetBox = '<article class="tweets-unit">' +
                        '<img src=' + val.user.profile_image_url + ' alt=""/>' +
                        '<p class="tweets-text">' + val.text + '</p>' + 
                        '<p class="tweets-user"><em>' + val.user.name + '</em></p>' +
                        '<p class="tweets-date">' + val.created_at + '</p>' +
-                       '</div>';
+                       '</article>';
           $('#tweets-box').append(tweetBox);
       });
     }); 
